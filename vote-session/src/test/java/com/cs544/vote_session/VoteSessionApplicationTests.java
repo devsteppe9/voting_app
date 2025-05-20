67ee70b7c7ae4943e77aba099b253eb95dbaf9ee
Jarkynbyek Japar <18748558+devsteppe9@users.noreply.github.com>
2025-05-20 16:20:21 -0500

    Initialize vote session application

---
+++ vote-session/src/test/java/com/cs544/vote_session/VoteSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs544.vote_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
